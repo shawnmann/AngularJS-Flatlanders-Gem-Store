416684c7abb3d912a3c05db0b321f5d4fe3e9594
shawnmann <shawn.mann@gmail.com>
2014-07-28 14:45:34 -0700

    JSON file

---
+++ products.js
@@ -0,0 +1,31 @@
+﻿(function () {
+
+    var app = angular.module('store-products', []);
+
+    app.directive('productTitle', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-panels.html',
+            controller: function () {
+                this.tab = 1;
+
+                this.selectTab = function (setTab) {
+                    this.tab = setTab;
+                };
+
+                this.isSelected = function (checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panel'
+        };
+    });
+
+})();
